96dda7b1d1dfec4b77aa48265b7216b72c84023d
Sylvain Guillet <sylvain.guillet@live.com>
2024-01-07 13:33:14 +0100

    Implement ephemeris command

--- IO.Astrodynamics.CLI/Commands/EphemerisCommand.cs
+++ IO.Astrodynamics.CLI/Commands/EphemerisCommand.cs
@@ -8,9 +8,15 @@ public class EphemerisCommand
     {
     }
 
-    [Cocona.Command("ephemeris", Description = "Compute ephemeris of given body")]
-    public Task Ephemeris([Argument(Description = "The name to display")] string name)
+    [Command("ephemeris", Description = "Compute ephemeris of given object")]
+    public Task Ephemeris(
+        [Argument(Description = "Object identifier")] int objectId, 
+        [Argument("Kernels directory path")] string kernelsPath,
+        [Option(shortName: 'b', Description = "Begin epoch")] string begin, 
+        [Option(shortName: 'e',Description = "End epoch")] string end,
+        [Option(shortName: 's',Description = "Step size in seconds")] double step,
+        [Option(shortName: 'f',Description = "Frame")] string frame,
+        [Option(shortName: 'a',Description = "Aberration")] string aberration)
     {
-        return Task.FromResult(() => Console.WriteLine($"Name : {name}"));
     }
 }
