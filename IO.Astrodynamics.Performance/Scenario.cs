cc31d3820c75da7821b895e516e63718ff40eaa8
Sylvain Guillet <sylvain.guillet@live.com>
2024-01-30 21:57:30 +0100

    add performance test

--- IO.Astrodynamics.Performance/Scenario.cs
+++ IO.Astrodynamics.Performance/Scenario.cs
@@ -1,15 +1,16 @@
 // Copyright 2023. Sylvain Guillet (sylvain.guillet@tutamail.com)
 
+using System.Numerics;
 using BenchmarkDotNet.Attributes;
 using IO.Astrodynamics.Body;
 using IO.Astrodynamics.Body.Spacecraft;
 using IO.Astrodynamics.Frames;
-using IO.Astrodynamics.Math;
 using IO.Astrodynamics.OrbitalParameters;
 using IO.Astrodynamics.Physics;
 using IO.Astrodynamics.Propagator.Forces;
 using IO.Astrodynamics.Propagator.Integrators;
 using IO.Astrodynamics.Time;
+using Vector3 = IO.Astrodynamics.Math.Vector3;
 
 namespace IO.Astrodynamics.Performance;
 
@@ -95,9 +96,35 @@ public class Scenario
         // var res = _integrator.Integrate(sv);
     }
 
-    [Benchmark(Description = "Propagator per orbit (GeoPotentials // Moon and sun perturbation // Atmospheric drag // Solar radiation) ")]
+    // [Benchmark(Description = "Propagator per orbit (GeoPotentials // Moon and sun perturbation // Atmospheric drag // Solar radiation) ")]
     public void Propagator()
     {
         var res = _propagator.Propagate();
+    }
+
+    [Benchmark(Description = "IO Vector", Baseline = true)]
+    public void Vector()
+    {
+        Vector3 vectorA = new Vector3(1.0, 2.0, 3.0);
+        Vector3 vectorB = new Vector3(10.0, 20.0, 30.0);
+        var res = vectorA * vectorB;
+        var dif = vectorA - vectorB;
+        var add = vectorA + vectorB + vectorA - vectorB;
+        var d = vectorA * 9.24;
+        var tot = dif * res + add + d;
+        var mag = tot.Magnitude();
+    }
+
+    [Benchmark(Description = "Numerics Vector")]
+    public void VectorNumerics()
+    {
+        var vectorA = new Vector<double>([0.0, 1.0, 2.0, 3.0]);
+        var vectorB = new Vector<double>([0.0, 10.0, 20.0, 30.0]);
+        var res = System.Numerics.Vector.Dot(vectorA, vectorB);
+        var dif = vectorA - vectorB;
+        var add = vectorA + vectorB + vectorA - vectorB;
+        var d = vectorA * 9.24;
+        var tot = dif * res + add + d;
+        var mag = System.Math.Sqrt(System.Numerics.Vector.Sum(tot * tot));
     }
 }
