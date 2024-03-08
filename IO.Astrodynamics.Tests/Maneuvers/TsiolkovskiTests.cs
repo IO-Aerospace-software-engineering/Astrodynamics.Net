c71d63f963e54dcbc0a86ace15279df13a70b9c7
Sylvain Guillet <sylvain.guillet@live.com>
2024-03-08 13:56:14 +0100

    Move Tsiolkovski equation (#81)

--- IO.Astrodynamics.Tests/Maneuvers/TsiolkovskiTests.cs
+++ IO.Astrodynamics.Tests/Maneuvers/TsiolkovskiTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using IO.Astrodynamics.Physics;
 using Xunit;
 
 namespace IO.Astrodynamics.Tests.Maneuvers
@@ -8,19 +9,19 @@ namespace IO.Astrodynamics.Tests.Maneuvers
         [Fact]
         public void DeltaM()
         {
-            Assert.Equal(1000.0, Maneuver.Maneuver.ComputeDeltaM(300.0, 3000.0, 1192.876320728679), 9);
+            Assert.Equal(1000.0, Tsiolkovski.DeltaM(300.0, 3000.0, 1192.876320728679), 9);
         }
 
         [Fact]
         public void DeltaT()
         {
-            Assert.Equal(TimeSpan.FromSeconds(10.0), Maneuver.Maneuver.ComputeDeltaT(300.0, 3000.0, 100.0, 1192.876320728679));
+            Assert.Equal(TimeSpan.FromSeconds(10.0), Tsiolkovski.DeltaT(300.0, 3000.0, 100.0, 1192.876320728679));
         }
 
         [Fact]
         public void DeltaV()
         {
-            double deltaV = Maneuver.Maneuver.ComputeDeltaV(300.0, 3000.0, 2000.0);
+            double deltaV = Tsiolkovski.DeltaV(300.0, 3000.0, 2000.0);
 
             Assert.Equal(1192.876320728679, deltaV);
         }
